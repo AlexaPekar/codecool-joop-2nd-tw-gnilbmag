0ba35b1e0614cc72c1b18fcda3ce4a6fe33d9c07
hreskonorbert <norberthresko@gmail.com>
2018-01-22 14:09:44 +0100

    Created the bases of the files

---
+++ src/Simulation.java
@@ -0,0 +1,16 @@
+// Writes a simulated round to a file
+//and loads the content of an instance 
+//of a Simulation and returns it
+
+
+public class Simulation{
+
+
+    public void generateData(){
+
+    }
+
+    public void load(){
+
+    }
+}
