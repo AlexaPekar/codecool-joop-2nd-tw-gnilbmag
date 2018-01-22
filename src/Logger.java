caa2e9abf1134b0d91c0b0fe2b0cd280cb02d9be
AlexaPekar <missdreambelle@gmail.com>
2018-01-22 14:09:28 +0100

    Created the bases of the files.

---
+++ src/Logger.java
@@ -0,0 +1,9 @@
+//It writes information to the console. Only this class can contain calls to System.out.println().
+
+public class Logger {
+
+    //using TimeStamp for example, Use java.util.Date for creating timestamps.
+    public void log(String type, String message) {
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//Starts the application. Able to configure the round of the simulations to run by taking an argument from the command line.
+
+public class Main {
+
+    public Simulation generateSimulation(int round) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
