37318cbd76b55201df2c44f43013aabf4e358b3d
AlexaPekar <missdreambelle@gmail.com>
2018-01-23 10:12:50 +0100

    Implemented collectWinningNumbers method in Simulator.java

--- src/Simulator.java
+++ src/Simulator.java
@@ -24,4 +24,58 @@ public class Simulator{
         }
 
     }
+
+    public int[] collectWinningNumbers(int[] numberOccurences) {
+        int first = 0, second = 0, third = 0, fourth = 0, fifth = 0, sixth = 0;
+        for (int i = 0; i < numberOccurences.length; i++) {
+                if (numberOccurences[i] > first) {
+                    sixth = fifth;
+                    fifth = fourth;
+                    fourth = third;
+                    third = second;
+                    second = first;
+                    first = numberOccurences[i];
+                }
+                else if (numberOccurences[i] > second) {
+                    sixth = fifth;
+                    fifth = fourth;
+                    fourth = third;
+                    third = second;
+                    second = i+1;
+                }
+                else if (numberOccurences[i] > third) {
+                    sixth = fifth;
+                    fifth = fourth;
+                    fourth = third;
+                    third = i+1;
+                }
+                else if (numberOccurences[i] > fourth) {
+                    sixth = fifth;
+                    fifth = fourth;
+                    fourth = i+1;
+                }
+                else if (numberOccurences[i] > fifth) {
+                    sixth = fifth;
+                    fifth = i+1;
+                }
+                else if (numberOccurences[i] > sixth) {
+                    sixth = i+1;
+                }
+        }
+        int [] winningNumbers = {first, second, third, fourth, fifth, sixth};
+        int [] foundWinningNumbers = new int[6];
+        for (int i = 0; i < numberOccurences.length; i++) {
+            for (int j = 0; j < winningNumbers.length; j++) {
+                if (numberOccurences[i] == winningNumbers[j]) {
+                    if (foundWinningNumbers[j] == 0) {
+                        foundWinningNumbers[j] = i+1;
+                        System.out.println(foundWinningNumbers[j]);
+                    }
+                }   
+            }
+        }
+        return foundWinningNumbers;
+    }
+
+
 }
